20d6b29e1e5dda5ed5a34595365d3c0e3695dfb0
Matheus Nascimento <matheusdrkwolf@gmail.com>
2019-05-30 17:33:46 -0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+            
+            double saldo = s.Saldo(textBox1.Text, textBox2.Text);
+            MessageBox.Show(saldo.ToString());
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+
+            bool deposito = s.Deposito(Double.Parse(textBox4.Text), textBox3.Text);
+            MessageBox.Show("Deposito Realizado com Sucesso");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+            bool saque = s.Saque(Double.Parse(textBox6.Text), textBox5.Text);
+            MessageBox.Show("Saque Realizado com Sucesso");
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+            bool cadastrar = s.CadastrarUsuario(textBox7.Text, textBox8.Text, textBox9.Text);
+            MessageBox.Show("Cadastro Realizado com Sucesso");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+            bool alterar = s.AlterarCadastro(textBox10.Text, textBox11.Text, textBox12.Text, textBox13.Text);
+            MessageBox.Show("Alterado com Sucesso");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Servico s = new Servico();
+            string consultar = s.ConsultarConta(textBox14.Text, textBox15.Text);
+            MessageBox.Show(consultar);
+        }
+    }
+}
---
+++ WindowsFormsApp1/Servico.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.ServiceReference1;
+
+namespace WindowsFormsApp1
+{
+    class Servico
+    {
+        public ServiceReference1.BankWSClient servico = new ServiceReference1.BankWSClient();
+
+      
+
+        //  public bool Cadastrar(String nome, String senha, String saldo)
+        // {
+        //       return servico.cadastrarUser(nome, senha, saldo);
+        //   }
+
+        public String nomeSaldo = "Matheus";
+        public String senhaSaldo = "23123";
+        public Double valor;
+        public String nome;
+        public String senha;
+        public String nomeAtual;
+        public String senhaAtual;
+
+        public double Saldo(String nomeSaldo, String senhaSaldo)
+        {
+            //Console.WriteLine("teste");
+            return servico.saldoUser(nomeSaldo, senhaSaldo);
+            //return 0;
+        }
+
+        public bool Deposito(Double valor, String nome)
+        {
+            return servico.deposito(valor, nome);
+        }
+
+        public bool Saque(Double valor, String nome)
+        {
+            return servico.saque(valor, nome);
+        }
+
+        public bool CadastrarUsuario(String nome, String senha, String valor)
+        {
+            return servico.cadastrarUser(nome, senha, valor);
+        }
+
+        public bool AlterarCadastro(String nomeAtual, String nome, String senhaAtual, String senha)
+        {
+            return servico.alterCadastro(nomeAtual, nome, senhaAtual, senha);
+        }
+
+        public string ConsultarConta(String nome, String senha)
+        {
+            return servico.consultUser(nome, senha);
+        }
+        
+
+    }
+}
